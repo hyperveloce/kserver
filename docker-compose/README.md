d9299bf1db116217ba5d414afd2f3034ae61d03f
dnikigai <david.nguyen@ikigaitech.com.au>
2024-04-01 16:38:32 +1100

    wip: docker-c

---
+++ docker-compose/README.md
@@ -0,0 +1,4 @@
+
+
+Base Docker-compose
+docker-compose -f docker-compose-base.yml up -d
--- install.sh
+++ install.sh
@@ -44,7 +44,7 @@ chown -R $username:$username /home/$username
 apt install nala -y
 
 # Installing Essential Programs
-apt install x11-xserver-utils unzip wget build-essential git samba smbclient cifs-utils neofetch neovim rsync xserver-xorg xterm -y
+apt install x11-xserver-utils unzip wget build-essential git samba smbclient cifs-utils neofetch neovim rsync xserver-xorg xterm docker-compose-y
 
 # # Beautiful bash
 # bash scripts/setup.sh
